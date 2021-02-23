89b05e5643837c5f724c7c4946395a48ad14f840
Steve O'Donovan <stephen.odonovancs@gmail.com>
2021-02-23 13:51:57 +0000

    Create node.sh

---
+++ k8s/node.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -x
+node=${1}
+nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}') 
+nodeSelector='"nodeSelector": { "kubernetes.io/hostname": "'${nodeName:?}'" },'
+podName=${USER}-nsenter-${node}
+kubectl run ${podName:?} --restart=Never -it --rm --image overriden --overrides '
+{
+  "spec": {
+    "hostPID": true,
+    "hostNetwork": true,
+    '"${nodeSelector?}"'
+    "tolerations": [{
+        "operator": "Exists"
+    }],
+    "containers": [
+      {
+        "name": "nsenter",
+        "image": "alpine:3.10.3",
+        "command": [
+          "nsenter", "-t", "1", "-m", "-u", "-i", "-n", "-p", "--", "bash"
+        ],
+        "stdin": true,
+        "tty": true,
+        "securityContext": {
+          "privileged": true
+        }
+      }
+    ]
+  }
+}' --attach "$@"
